eadf3535984834bafd75d8a338fb1a9bc051da38
krnn <knag@tuta.io>
2021-04-16 08:22:18 +0530

    infra image slider

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -13,3 +13,36 @@ window.toggleMenu = () => {
 if (module.hot) {
   module.hot.accept()
 }
+
+
+window.openTab = (evt, infraName) => {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("infra__box__tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tab-btn");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(infraName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+window.slide = (reelId, dir) => {
+  let reel = document.getElementById(reelId)
+  let ch = reel.children.length - 1
+
+  console.log(typeof i === 'undefined');
+  if (typeof i === 'undefined') {
+    console.log('init');
+    var i = 0
+  }
+  
+  if (dir === 'r' && i < ch) {i++}
+  if (dir === 'l' && i > 1) {i--}
+  reel.style.marginLeft = (-61 * i) + "rem";
+  console.log(i < ch, dir, ch, i);
+  
+}
